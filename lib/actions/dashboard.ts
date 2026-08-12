05496dbc6ff386b1468ba804969a89fce0ec0b68
Gabriel Nardes <lightsirius3@gmail.com>
2026-08-12 11:49:22 -0300

    fix(dashboard): fix name of database columns in getDriverFuelData

--- lib/actions/dashboard.ts
+++ lib/actions/dashboard.ts
@@ -2,7 +2,7 @@
 import {ResponseType} from "@/lib/types";
 import { prisma } from "../prisma";
 import { DateSchema, DateType } from "@/schemas/date.schema";
-import { FuelBarChartItemType, KPIItemType } from "@/schemas/dashboard.schema";
+import { FuelBarChartItemType, FuelEfficiencyByVehicleType, KPIItemType } from "@/schemas/dashboard.schema";
 
 export const getKPIData = async ({from, to}: {from?: DateType; to?: DateType}): Promise<ResponseType<KPIItemType[]>> => {
         const vFrom = DateSchema.safeParse(from);
@@ -87,8 +87,8 @@ export const getDriverFuelData = async ({from, to}: {from?: DateType; to?: DateT
             SELECT 
                 d.name AS "name",
                 COALESCE(SUM(f.liters), 0)::float AS "liters"
-            FROM "Fueling" f
-            JOIN "Driver" d ON d.id = f."driverId"
+            FROM "fuelings" f
+            JOIN "drivers" d ON d.id = f."driverId"
             WHERE f."createdAt" >= ${fromDate} 
                 AND f."createdAt" <= ${toDate}
             GROUP BY d.id, d.name
@@ -102,3 +102,42 @@ export const getDriverFuelData = async ({from, to}: {from?: DateType; to?: DateT
         return {success: false, error: 'Erro ao buscar dados do dashboard'}
     }
 }
+
+// // export const getFuelEfficiencyByCarData = async ({from, to}: {from?: DateType; to?: DateType}): Promise<ResponseType<FuelEfficiencyByVehicleType[]>> => {
+// export const getFuelEfficiencyByCarData = async ({from, to}: {from?: DateType; to?: DateType}) => {
+//             const vFrom = DateSchema.safeParse(from);
+//         const vTo = DateSchema.safeParse(to);
+    
+//         const toDate = vTo.success ? vTo.data : new Date();
+    
+//         const defaultFromDate = new Date(toDate);
+//         defaultFromDate.setDate(defaultFromDate.getDate() - 30);
+//         defaultFromDate.setHours(0, 0, 0, 0); 
+    
+//         const fromDate = vFrom.success ? vFrom.data : defaultFromDate;
+//         toDate.setHours(23, 59, 59, 999);
+
+//     try {
+//         const data = await prisma.$queryRaw<FuelEfficiencyByVehicleType[]>`
+//             SELECT
+//                 v.brand AS "vehicle brand",
+//                 v.model AS "vehicle model",
+//                 v.plate AS "vehicle plate",
+//                 v.year AS "vehicle year",
+//                 d.name AS "driver",
+//                 ROUND(AVG(f."fuelEfficiency")::numeric, 2)::float AS "efficiency"
+//             FROM "fuelings" f
+//             JOIN "vehicles" v ON v.id = f."vehicleId"
+//             JOIN "drivers" d ON d.id = f."driverId"
+//             WHERE f."createdAt" >= ${fromDate}
+//                 AND f."createdAt" <= ${toDate}
+//             GROUP BY v.brand, v.model, v.plate, v.year, d.name
+//             ORDER BY "efficiency" DESC;
+//         `
+//         console.log('FuelEfficiencyByCarData:', data);
+//         return {success: true, data: data.map(item => ({car: item.vehicle, [item.driver]: item.efficiency}))}
+//     } catch (err) {
+//         console.log(err);
+//         return {success: false, error: 'Erro ao buscar dados do dashboard'};
+//     }
+// }
