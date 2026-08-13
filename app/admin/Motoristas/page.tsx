14a0e8fe1b58114e4516237962bc2629ac594d23
Gabriel Nardes <lightsirius3@gmail.com>
2026-08-13 14:24:54 -0300

    fix(driver): fix page h1 name

--- app/admin/Motoristas/page.tsx
+++ app/admin/Motoristas/page.tsx
@@ -11,7 +11,7 @@ const Motoristas = async () => {
 
   return (
     <>
-      <HeaderTemplate title="Contratos">
+      <HeaderTemplate title="Motoristas">
         {isAdmin && <AddMotorista />}
       </HeaderTemplate>
 
